c19de1b062035b4aae911f878e3dd28fac09d866
Debashish Saha <debashish.deep74@qmail.cuny.edu>
2016-04-27 22:19:06 -0400

    project started

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : project.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!! 22 21 51!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
